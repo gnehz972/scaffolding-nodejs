46d56cf18b8462a6861f264f4dbb6bf37e95aff8
gnehz972 <zoz20042008@gmail.com>
2024-03-07 22:35:29 +0800

    test migration

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,46 @@
+import { Schema, model, connect } from "mongoose";
+
 import config from "./config";
 import { getServer } from "./server";
 import { logger } from "./util/logger";
+import MongooseClient from "./clients/MongooseClient";
+import MigrationClient from "./clients/MigrationClient";
+
+// 1. Create an interface representing a document in MongoDB.
+interface IUser {
+  name: string;
+  email: string;
+  avatar?: string;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  avatar: String,
+});
+
+// 3. Create a Model.
+const User = model<IUser>("User", userSchema);
+
+const startDb = async () => {
+  await MongooseClient.connect();
+  await MigrationClient.connect();
+
+  // await connect('mongodb://127.0.0.1:27017/test');
+  logger.info(`mongodb started on port:27017`);
+
+  // const user = new User({
+  //   name: 'Bill',
+  //   email: 'bill@initech.com',
+  //   avatar: 'https://i.imgur.com/dM7Thhn.png'
+  // });
+  // await user.save();
+
+  // console.log(user.email); //
+};
 
 getServer().listen(config.port, () => {
   logger.info(`Server started on port:${config.port}`);
+  startDb().catch((err) => console.log(err));
 });
---
+++ src/clients/MongooseClient.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+import type { MongooseError } from "mongoose";
+
+// In case you using mongoose 6
+// https://mongoosejs.com/docs/guide.html#strictQuery
+mongoose.set("strictQuery", false);
+
+mongoose.connection.on("error", (err: MongooseError) => {
+  console.error(err.message);
+});
+
+mongoose.connection.on("connecting", () => {
+  console.info("Connecting to MongoDB...");
+});
+
+mongoose.connection.on("connected", () => {
+  console.info("Connected to MongoDB");
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.info("Disconnected from MongoDB");
+});
+
+mongoose.connection.on("reconnect", () => {
+  console.info("Reconnecting to MongoDB...");
+});
+
+export default {
+  connect: async () => {
+    await mongoose.connect(process.env.MONGO_URI ?? "mongodb://localhost:27017/express", { autoIndex: true });
+    return mongoose;
+  },
+  isConnected: () => Number(mongoose.connection.readyState) === 1,
+};
